e9932197b50bdf4ff0ddc0be0204f7ea59af9721
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-07-30 23:38:42 +0100

    feat: create tutor class add attributes and add attribute to Student

--- app/src/main/java/Student.java
+++ app/src/main/java/Student.java
@@ -2,6 +2,7 @@ public class Student {
     private int id;
     private String firstName;
     private String lastName;
+    private String email;
     private int yearGroup;
     private String subject;
 }
---
+++ app/src/main/java/Tutor.java
@@ -0,0 +1,8 @@
+public class Tutor {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private float hourlyWage;
+
+}
